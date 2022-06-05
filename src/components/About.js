fa0ec073109ae602ff5fd460524e0f49923e20d1
Rand Al'Thor <alexanderhogle@gmail.com>
2022-06-05 14:12:41 -0500

    home styling and about section outline

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,35 @@
+import profile from '../img/me-img.jpeg';
+
 const About = () => {
-  <>
-    <p>
-      I am a Full-Stack Web Developer located in Austin, TX. Creating is what I
-      love to do, namely websites.
-    </p>
-    <p>
-      Other things I love to create include music, journaling, and fantasy
-      worlds in Dungeons & Dragons.
-    </p>
-  </>;
+  return (
+    <section className="container">
+      <div className="landing-inner">
+        <h1 className="large">About</h1>
+        <div className="about-container">
+          <div className="about-text-container">
+            <p>
+              I am a Full-Stack Web Developer located in Austin, TX. Creating is
+              what I love to do, namely websites.
+            </p>
+            <p>
+              Other things I love to create include music and fantasy worlds in
+              Dungeons & Dragons.
+            </p>
+            <ul className="skills-list">
+              <li>- JavaScript (ES6+)</li>
+              <li>- React</li>
+              <li>- Node.js</li>
+              <li>- GraphQL</li>
+              <li>- MongoDB</li>
+              <li>- React Native</li>
+            </ul>
+          </div>
+
+          <img src={profile} alt="Me." />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,5 +1,23 @@
 const Portfolio = () => {
-
-}
+  return (
+    <section className="container">
+      <div className="landing-inner">
+        <h1 className="large">About</h1>
+        <div className="about-container">
+          <div className="about-text-container">
+            <p>
+              I am a Full-Stack Web Developer located in Austin, TX. Creating is
+              what I love to do, namely websites.
+            </p>
+            <p>
+              Other things I love to create include music and fantasy worlds in
+              Dungeons & Dragons.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Portfolio;
