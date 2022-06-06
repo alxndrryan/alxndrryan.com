eebc03e2199e0a4c67d173f67e12e22c37267dd7
Rand Al'Thor <alexanderhogle@gmail.com>
2022-06-06 17:28:09 -0500

    adds margin-top to button and adds line for styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,8 +6,9 @@ const Home = () => {
           <h1 className="x-large">Hi, my name is Alexander Hogle.</h1>
           <h2 className="large">Full-Stack Web Developer.</h2>
           <p className="lead">Always building something new.</p>
-          <div>
-            <a className="btn btn-primary my-1">Portfolio</a>
+          <div className="port-btn">
+            <a className="btn btn-primary my-3">Portfolio</a>
+            <div className="line-1"></div>
           </div>
         </div>
       </div>
