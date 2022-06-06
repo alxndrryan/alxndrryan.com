9b3d500374473f2001d4dd7bb083447806710c41
Rand Al'Thor <alexanderhogle@gmail.com>
2022-06-06 17:12:52 -0500

    left-align home text and stylize button

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,13 @@ const Home = () => {
   return (
     <section className="landing">
       <div className="dark-overlay">
-        <div className="landing-inner">
+        <div className="landing-inner landing-inner-home">
           <h1 className="x-large">Hi, my name is Alexander Hogle.</h1>
           <h2 className="large">Full-Stack Web Developer.</h2>
+          <p className="lead">Always building something new.</p>
+          <div>
+            <a className="btn btn-primary my-1">Portfolio</a>
+          </div>
         </div>
       </div>
     </section>
