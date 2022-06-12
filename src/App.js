7e73529c679168733987dbaf17921de2396bbc2b
Rand Al'Thor <alexanderhogle@gmail.com>
2022-06-11 19:03:05 -0500

    adds socials component with some styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faFile, faAward } from '@fortawesome/free-solid-svg-icons';
+
 import About from './components/About';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import Portfolio from './components/Portfolio';
+import Socials from './components/Socials';
 
 import './css/App.min.css';
+
+library.add(fab, faFile, faAward);
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Socials />
       <Home />
       <About />
       <Portfolio />
---
+++ src/components/Socials.js
@@ -0,0 +1,33 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const Socials = () => {
+  return (
+    <div className="socials-container">
+      <ul>
+        <li>
+          <div className="socials-line"></div>
+        </li>
+        <li>
+          <a>
+            <FontAwesomeIcon icon="fa-brands fa-github" />
+          </a>
+        </li>
+        <li>
+          <FontAwesomeIcon icon="fa-brands fa-linkedin" />
+        </li>
+        <li>
+          <FontAwesomeIcon icon="fa-brands fa-twitter" />
+        </li>
+        <li>
+          <FontAwesomeIcon icon="fa-solid fa-file" />
+        </li>
+        <li>
+          <FontAwesomeIcon icon="fa-solid fa-award" />
+        </li>
+        <li></li>
+      </ul>
+    </div>
+  );
+};
+
+export default Socials;
