b44ae76c9c8ea0ed631f57e32f5d8a8a58657711
Rand Al'Thor <alexanderhogle@gmail.com>
2022-06-13 17:15:17 -0500

    1 hour styling a socials section for the mobile footer :)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,28 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 const Footer = () => {
   return (
     <footer className="footer">
+      <ul className="footer-socials hide-lg">
+        <li>
+          <a>
+            <FontAwesomeIcon icon="fa-brands fa-github" />
+          </a>
+        </li>
+        <li>
+          <FontAwesomeIcon icon="fa-brands fa-linkedin" />
+        </li>
+        <li>
+          <FontAwesomeIcon icon="fa-brands fa-twitter" />
+        </li>
+        <li>
+          <FontAwesomeIcon icon="fa-solid fa-file" />
+        </li>
+        <li>
+          <FontAwesomeIcon icon="fa-solid fa-award" />
+        </li>
+      </ul>
+
       <p>Alexander Hogle ©2022</p>
     </footer>
   );
