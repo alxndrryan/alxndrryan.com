a4e3999790378bc0f45c0509a15ec97c0ccd05c2
Rand Al'Thor <alexanderhogle@gmail.com>
2022-06-14 17:38:14 -0500

    functional socials links on component and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,27 +5,47 @@ const Footer = () => {
     <footer className="footer">
       <ul className="footer-socials hide-lg">
         <li>
-          <a href="#" className="socials-btn">
+          <a
+            target="_blank"
+            href="https://github.com/alxndrryan"
+            className="socials-btn"
+          >
             <FontAwesomeIcon icon="fa-brands fa-github" />
           </a>
         </li>
         <li>
-          <a href="#" className="socials-btn">
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/in/alexander-hogle/"
+            className="socials-btn"
+          >
             <FontAwesomeIcon icon="fa-brands fa-linkedin" />
           </a>
         </li>
         <li>
-          <a href="#" className="socials-btn">
+          <a
+            target="_blank"
+            href="https://twitter.com/alxndrryan"
+            className="socials-btn"
+          >
             <FontAwesomeIcon icon="fa-brands fa-twitter" />
           </a>
         </li>
         <li>
-          <a href="#" className="socials-btn">
+          <a
+            target="_blank"
+            href="https://drive.google.com/file/d/1HiF-alFoNPCSx97_iywN84fQRC_syTvq/view?usp=sharing"
+            className="socials-btn"
+          >
             <FontAwesomeIcon icon="fa-solid fa-file" />
           </a>
         </li>
         <li>
-          <a href="#" className="socials-btn">
+          <a
+            target="_blank"
+            href="https://drive.google.com/file/d/1JQNyH1WW3EMYmm2dEBMudAxJF-HSTZ6A/view?usp=sharing"
+            className="socials-btn"
+          >
             <FontAwesomeIcon icon="fa-solid fa-award" />
           </a>
         </li>
--- src/components/Socials.js
+++ src/components/Socials.js
@@ -8,27 +8,47 @@ const Socials = () => {
           <div className="socials-line"></div>
         </li>
         <li>
-          <a href="#" className="socials-btn">
+          <a
+            target="_blank"
+            href="https://github.com/alxndrryan"
+            className="socials-btn"
+          >
             <FontAwesomeIcon icon="fa-brands fa-github" />
           </a>
         </li>
         <li>
-          <a href="#" className="socials-btn">
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/in/alexander-hogle/"
+            className="socials-btn"
+          >
             <FontAwesomeIcon icon="fa-brands fa-linkedin" />
           </a>
         </li>
         <li>
-          <a href="#" className="socials-btn">
+          <a
+            target="_blank"
+            href="https://twitter.com/alxndrryan"
+            className="socials-btn"
+          >
             <FontAwesomeIcon icon="fa-brands fa-twitter" />
           </a>
         </li>
         <li>
-          <a href="#" className="socials-btn">
+          <a
+            target="_blank"
+            href="https://drive.google.com/file/d/1HiF-alFoNPCSx97_iywN84fQRC_syTvq/view?usp=sharing"
+            className="socials-btn"
+          >
             <FontAwesomeIcon icon="fa-solid fa-file" />
           </a>
         </li>
         <li>
-          <a href="#" className="socials-btn">
+          <a
+            target="_blank"
+            href="https://drive.google.com/file/d/1JQNyH1WW3EMYmm2dEBMudAxJF-HSTZ6A/view?usp=sharing"
+            className="socials-btn"
+          >
             <FontAwesomeIcon icon="fa-solid fa-award" />
           </a>
         </li>
