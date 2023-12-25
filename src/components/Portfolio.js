8b2e05577bea7c8430cb4a030697a015153de90c
Alexander Hogle <alexanderhogle@gmail.com>
2023-12-24 23:04:43 -0600

    adds react native jobs to portfolio section

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -11,6 +11,17 @@ const Portfolio = () => {
           <ul className="projects-grid">
             <li className="project-item">
               <Project
+                github="https://github.com/alxndrryan/react_native_jobs"
+                website="exp://u.expo.dev/update/6c232df9-54b3-4414-871f-b7297aad12dc"
+                title="React Native Jobs"
+                tech1="React"
+                tech2="MongoDB"
+                tech3="Expo"
+                about="Mobile application that allows users to search and apply for jobs using Rapid API."
+              />
+            </li>
+            <li className="project-item">
+              <Project
                 github="https://github.com/alxndrryan/dev-connector"
                 website="https://secure-scrubland-55057.herokuapp.com/"
                 title="DevConnector"
